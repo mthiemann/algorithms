323d9dfd4e4ffb65de3e12443aaa5f194d473bee
Marc Tim Thiemann <marc.t.thiemann@gmail.com>
2017-05-09 21:51:24 -0700

    Add MergeSort

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ A collection of personally programmed algorithms for quick lookup and reuse
 
 ## Recursion & Backtracking
 - [NQueens](Recursion%20%26%20Backtracking/NQueens.java)
+
+## Sorting
+- [MergeSort](Sorting/MergeSort.java)
---
+++ Sorting/MergeSort.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import Helpers.Timer;
+
+
+class MergeSort {
+
+  public static void mergeSort(int[] arr) {
+    mergeSort(arr, 0, arr.length - 1);
+  }
+
+  public static void mergeSort(int[] arr, int start, int end) {
+    if (start == end) return;
+
+    int mid = start + (int) Math.floor((end - start) / 2);
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+
+    // merge into array using previous start and end values
+    int indexOne = start;
+    int indexTwo = mid + 1;
+    int index = 0;
+    int[] sortedArr = new int[end - start + 1];
+
+    // sort subarrays from last divide-conquer call into sortedArr
+    while (indexOne <= mid || indexTwo <= end) {
+      if (indexTwo > end) {
+        sortedArr[index++] = arr[indexOne++];
+        continue;
+      }
+
+      if (indexOne > mid) {
+        sortedArr[index++] = arr[indexTwo++];
+        continue;
+      }
+
+      sortedArr[index++] = (arr[indexOne] <= arr[indexTwo]) ? arr[indexOne++] : arr[indexTwo++];
+    }
+
+    // update main array: assign sorted subarray to corresponding part in main array
+    for (int i = 0; i < sortedArr.length; i++) {
+      arr[start + i] = sortedArr[i];
+    }
+  }
+
+  public static void main(String args[] ) throws Exception {
+    Scanner s = new Scanner(System.in);
+    int N = s.nextInt();
+    int[] arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = s.nextInt();
+    }
+
+    Timer timer = new Timer();
+    timer.start();
+    mergeSort(arr);
+    timer.stop();
+    timer.printDuration();
+
+    for (int i = 0; i < N; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
