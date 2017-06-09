0aa64ce9543cc1c2d78ba6776ac77590d9c8ee82
Marc Tim Thiemann <marc.t.thiemann@gmail.com>
2017-06-09 14:33:37 -0700

    Improve BST implementation
    - simpler and more concise code for adding elements
    - cleaner code for getEdgeLeaf
    - factored out the node class into the tree interface so that it is available for all tree implementations

--- src/DataStructures/Trees/BST.java
+++ src/DataStructures/Trees/BST.java
@@ -6,62 +6,33 @@ package DataStructures.Trees;
  */
 public class BST<E extends Comparable<E>> implements Tree<E> {
 
-  private Node<E> root;
-  private int size;
+  Node<E> root;
+  int size;
 
-  private class Node<E> {
-
-    private E value;
-    private Node<E> left;
-    private Node<E> right;
-
-    public Node(E value) {
-      this(value, null, null);
-    }
-
-    public Node(E value, Node<E> left, Node<E> right) {
-      this.value = value;
-      this.left = left;
-      this.right = right;
-    }
-
-    public boolean isLeaf() {
-      return this.left == null && this.right == null;
-    }
-  }
-
-  public BST() {
+  BST() {
     this.root = null;
     this.size = 0;
   }
 
-  public void add(E value) {
-    add(value, this.root, null, false);
+  public boolean add(E value) {
+    int previousSize = this.size();
+    this.root = add(value, this.root);
+    return this.size() > previousSize;
   }
 
-  private void add(E value, Node<E> node, Node<E> parent, boolean left) {
-
+  Node<E> add(E value, Node<E> node) {
     if (node == null) {
+      node = new Node<E>(value);
       this.size++;
-      Node<E> nodeToInsert = new Node<E>(value);
-      if (parent == null) {
-        this.root = nodeToInsert;
-      } else {
-        if (left) {
-          parent.left = nodeToInsert;
-        } else {
-          parent.right = nodeToInsert;
-        }
+    } else {
+      if (node.value.compareTo(value) > 0) {
+        node.left = this.add(value, node.left);
+      } else if (node.value.compareTo(value) < 0) {
+        node.right = this.add(value, node.right);
       }
-      return;
     }
 
-    int comparison = value.compareTo(node.value);
-    if (comparison > 0) {
-      add(value, node.right, node, false);
-    } else if (comparison < 0) {
-      add(value, node.left, node, true);
-    }
+    return node;
   }
 
   public boolean remove(E value) {
@@ -121,8 +92,6 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
   }
 
   public boolean contains(E value) {
-    if (this.root == null) return false;
-
     Node<E> node = this.root;
 
     while (node != null) {
@@ -163,11 +132,9 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
     while (true) {
       nextNode = (right) ? node.right : node.left;
 
-      if (nextNode == null) {
-        return node.value;
-      } else {
-        node = nextNode;
-      }
+      if (nextNode == null) return node.value;
+
+      node = nextNode;
     }
   }
 }
--- src/DataStructures/Trees/Tree.java
+++ src/DataStructures/Trees/Tree.java
@@ -1,14 +1,35 @@
 package DataStructures.Trees;
 
+import DataStructures.Collection;
+
 public interface Tree<E extends Comparable<E>> {
 
-  void add(E element);
+  class Node<E> {
+
+    E value;
+    Node<E> left;
+    Node<E> right;
+    int height;
+
+    Node(E value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+      this.height = 0;
+    }
+
+    boolean isLeaf() {
+      return this.left == null && this.right == null;
+    }
+    int getLeftHeight() { return (this.left != null) ? this.left.height : -1; }
+    int getRightHeight() { return (this.right != null) ? this.right.height : -1; }
+    int getBalance() { return getLeftHeight() - getRightHeight(); }
+    void updateHeight() { this.height = Math.max(getLeftHeight(), getRightHeight()) + 1; }
+  }
+
+  boolean add(E element);
 
   boolean remove(E element);
 
   boolean contains(E element);
-
-  int size();
-
-  // possible extensions: toArray
 }
